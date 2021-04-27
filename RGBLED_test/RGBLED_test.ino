b36bbc042562169b0417a49e1af45f8be0acc979
sinyoon <sinyoon0580@gmail.com>
2021-04-27 17:21:52 +0900

    led불량 테스트

---
+++ RGBLED_test/RGBLED_test.ino
@@ -0,0 +1,79 @@
+#include<SoftwareSerial.h>
+
+SoftwareSerial TestSerial(10, 11);
+
+void setup() {
+  for (int i = 2; i <= 51; i++) {
+    if(i == 11){
+      i = 22;
+    }
+    pinMode(i, OUTPUT);
+  }
+  Serial.begin(9600);
+
+
+  Serial.println("Hello World!");
+}
+
+void loop() { //코드를 무한반복합니다.
+  for (int i = 2; i <= 51; i+=3) {
+    if(i == 11){
+      i = 22;
+    }
+    red(i, i + 1, i + 2);
+    Serial.println(i);
+    delay(1000);
+  }
+  delay(1000);
+  for (int i = 2; i <= 51; i+=3) {
+    if(i == 11){
+      i = 22;
+    }
+    green(i, i + 1, i + 2);
+    Serial.println(i);
+    delay(1000);
+  }
+  delay(1000);
+  for (int i = 2; i <= 51; i+=3) {
+    if(i == 11){
+      i = 22;
+    }
+    blue(i, i + 1, i + 2);
+    Serial.println(i);
+    delay(1000);
+  }
+  delay(1000);
+  for (int i = 2; i <= 51; i+=3) {
+    if(i == 11){
+      i = 22;
+    }
+    yellow(i, i + 1, i + 2);
+    Serial.println(i);
+    delay(1000);
+  }
+  delay(1000);
+}
+
+void red(int redPin, int greenPin, int bluePin) {
+  digitalWrite(redPin, HIGH);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+}
+
+void green(int redPin, int greenPin, int bluePin) {
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, HIGH);
+  digitalWrite(bluePin, LOW);
+}
+
+void blue(int redPin, int greenPin, int bluePin) {
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, HIGH);
+}
+
+void yellow(int redPin, int greenPin, int bluePin) {
+  digitalWrite(redPin, HIGH);
+  digitalWrite(greenPin, HIGH);
+  digitalWrite(bluePin, LOW);
+}
