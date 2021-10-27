609f43ad559b286a3c17cc530180c364b3981884
sinyoon <sinyoon0580@gmail.com>
2021-10-27 22:53:04 +0900

    mainboard다듬기

--- mainboard/mainboard.ino
+++ mainboard/mainboard.ino
@@ -1,10 +1,25 @@
 #include<SoftwareSerial.h>
 
+#define MainboardSerial Serial1
+
 SoftwareSerial Line1Serial(10, 11);
 SoftwareSerial Line2Serial(12, 13);
 SoftwareSerial Line3Serial(14, 15);
 SoftwareSerial Line4Serial(16, 17);
-SoftwareSerial MainboardSerial(18, 19);
+
+int lnad[11][11] = {
+  { "start", "taipei", "golden_key", "beijing", "manila", "jeju_island", "singapore", "golden_key", "cairo", "istanbul", "island"},
+  ( "seoul", " ", " ", " ", " ", " ", " ", " ", " ", " ", "athens"},
+  ( "welfarefund", " ", " ", " ", " ", " ", " ", " ", " ", " ", "golden_key"},
+  ( "new_york", " ", " ", " ", " ", " ", " ", " ", " ", " ", "copenhagen"},
+  ( "london", " ", " ", " ", " ", " ", " ", " ", " ", " ", "stockholm"},
+  ( "golden_key", " ", " ", " ", " ", " ", " ", " ", " ", " ", "concorde"},
+  ( "rome", " ", " ", " ", " ", " ", " ", " ", " ", " ", "bern"},
+  ( "paris", " ", " ", " ", " ", " ", " ", " ", " ", " ", "golden_key"},
+  ( "columbia", " ", " ", " ", " ", " ", " ", " ", " ", " ", "berlin"},
+  ( "tokyo", " ", " ", " ", " ", " ", " ", " ", " ", " ", "athens"},
+  ( "spacetravel", "madrid", "queen_elizabeth", "lisbon", "hawaii", "busan", "sydney", "sao_paulo", "golden_key", "buenosaires", "welfarefund"},
+}
 
 int cnt = 1;
 
@@ -16,36 +31,56 @@ void setup() {
   MainboardSerial.begin(9600);
 }
 
-void setLine(int buildingNum, int player){
-  if(1 <= buildingNum && buildingNum <= 8){
-      Line1Serial.println(player);
-      Line1Serial.println(buildingNum); 
-    } else if(9 <= buildingNum && buildingNum <= 18){
-      Line2Serial.println(player);
-      Line2Serial.println(buildingNum);
-    } else if(19 <= buildingNum && buildingNum <= 28){
-      Line3Serial.println(player);
-      Line3Serial.println(buildingNum);
-    } else {
-      Line4Serial.println(player);
-      Line4Serial.println(buildingNum);
-    }
-}
-
 void loop() { //코드를 무한반복합니다.
   
   if (MainboardSerial.available()) {
-    if(cnt == 1){
-      int player = (int)MainboardSerial.read();
-      cnt++;
-    } else {
-      int information = (int)MainboardSerial.read();
-      if(information == 0){
-        cnt = 1;
-      }
-      setLine(information, player);
+    String inString = Serial.readStringUntil('\n');
+    int inStringLength = inString.length();
+    int blank = inString.indexOf(' ');
+    String first = inString.substring(0, blank);
+    if (first == "B"){
+      inString = inString.substring(blank + 1, inStringLength);
+      split(inString);
+    }
+    if (first == "L"){
+      
     }
   }
   delay(1000);
+}
 
+void setLine(int x, y){
+  if(x == 0 && 0 <= y && y < 11){
+      Line1Serial.println(land[x][y]);
+    } else if(0 <= x && x < 11 && y == 10){
+      Line2Serial.println(land[x][y]);
+    } else if(x == 10 && 0 <= y && y < 11){
+      Line3Serial.println(land[x][y]);
+    } else {
+      Line4Serial.println(land[x][y]);
+    }
+}
+
+void split(String inString){
+  while(true){
+      int blank = inString.indexOf(' ');
+      int inStringLength = inString.length();
+      if(blank == -1){
+        data = inString.toInt();
+        sendData(data);
+        break;
+      }
+      String data = inString.substring(0, blank);
+      sendData(data);
+      inString = inString.substring(blank + 1, inStringLength);
+      delay(100);
+  }
+}
+
+void sendData(String data){
+  int blank = data.indexOf(',');
+  int dataLength = data.length();
+  int x = data.substring(0, blank).toInt();
+  int y = data.substring(blank + 1, dataLength).toInt();
+  setLine(x, y);
 }
--- mainboard_2/mainboard_2.ino
+++ mainboard_2/mainboard_2.ino
@@ -1,67 +1,35 @@
 #include<SoftwareSerial.h>
+#define MainboardSerial Serial1
+#define DiceSerial Serial2
 
-SoftwareSerial MainboardSerial(18, 19);
-SoftwareSerial RaspiSerial(10, 11);
 SoftwareSerial StepmotorSerial(12, 13);
 
 void setup() {
   Serial.begin(9600);
+  MainboardSerial.begin(9600);
+  DiceSerial.begin(9600);
   while (!Serial) {
     ; //시리얼통신이 연결되지 않았다면 코드 실행을 멈추고 무한 반복
   }
-  MainboardSerial.begin(9600);
-  RaspiSerial.begin(9600);
   StepmotorSerial.begin(9600);
 }
 
-void sendPositionInfo(String information){
-  while(true){
-      int blank = information.indexOf(' ');
-      int inStringLength = information.length();
-      if(blank == -1){
-        int lastInformation = information.toInt();
-        MainboardSerial.println(lastInformation);
-        break;
-      }
-      String first = information.substring(0, blank);
-      int positionInfo = first.toInt();
-      information = information.substring(blank + 1, inStringLength);
-      StepmotorSerial.println(positionInfo);
-      delay(100);
-    }
-}
-
-void sendBuildingInfo(String information){
-  while(true){
-      int blank = information.indexOf(' ');
-      int inStringLength = information.length();
-      if(blank == -1){
-        int lastInformation = information.toInt();
-        MainboardSerial.println(lastInformation);
-        break;
-      }
-      String first = information.substring(0, blank);
-      int buildingInfo = first.toInt();
-      information = information.substring(blank + 1, inStringLength);
-      MainboardSerial.println(buildingInfo);
-      delay(100);
-  }
-}
-
 void loop() { //코드를 무한반복합니다.
-  
-  if (RaspiSerial.available()) {
-    String inString = RaspiSerial.readStringUntil('\n');
+  if (DiceSerial.available()){
+    int diceNum = DiceSerial.readString().toInt();
+    Serial.println(diceNum);
+  }
+  if (Serial.available()) {
+    String inString = Serial.readStringUntil('\n');
     int blank = inString.indexOf(' ');
-    int inStringLength = inString.length();
     String first = inString.substring(0, blank);
-    inString = inString.substring(blank + 1, inStringLength);
     
     if( first == "M" ){
-      sendPositionInfo(inString);
-    }
-    else if( first == "L" ){
-      sendBuildingInfo(inString);
+      StepmotorSerial.println(information);
+    } else if( first == "L" ){
+      MainboardSerial.println(information);
+    } else if( first == "B"){
+      MainboardSerial.println(information);
     }
   }
   delay(1000);
